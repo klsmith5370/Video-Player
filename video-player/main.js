0be655ee48590d2802c3fa056ff8919540dad78c
Kourtney Smith <klsmith5370@gmail.com>
2024-02-28 17:37:17 -0500

    Added functionality for play/pause

--- video-player/main.js
+++ video-player/main.js
@@ -1,1 +1,28 @@
+"use strict";
 
+const playNpauseBtn = document.querySelector("#play-pause");
+const video = document.querySelector("video");
+
+// function playNpauseFn() {
+//   video.paused ? video.play() : video.pause();
+// }
+
+function playNpauseFn() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.paused();
+  }
+}
+
+function updatePlayNPauseIcon() {
+  const icon = playNpauseBtn.querySelector("i");
+  icon.textContent = "";
+
+  icon.textContent = video.paused ? "play_arrow" : "paused";
+}
+
+video.addEventListener("play", updatePlayNPauseIcon);
+video.addEventListener('click', playNpauseFn)
+video.addEventListener("pause", updatePlayNPauseIcon);
+playNpauseBtn.addEventListener("click", playNpauseFn);
