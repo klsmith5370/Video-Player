d88d0fbdf44a86cc74929ffe3a5881de83e26c00
Kourtney Smith <klsmith5370@gmail.com>
2024-02-29 16:16:58 -0500

    Rewind and fast-forward functionality added

--- video-player/main.js
+++ video-player/main.js
@@ -1,19 +1,20 @@
 "use strict";
 
+// Play & Pause Functionality
 const playNpauseBtn = document.querySelector("#play-pause");
 const video = document.querySelector("video");
 
-// function playNpauseFn() {
-//   video.paused ? video.play() : video.pause();
-// }
-
 function playNpauseFn() {
-  if (video.paused) {
-    video.play();
-  } else {
-    video.paused();
-  }
+  video.paused ? video.play() : video.pause();
 }
+
+// function playNpauseFn() {
+//   if (video.paused) {
+//     video.play();
+//   } else {
+//     video.paused();
+//   }
+// }
 
 function updatePlayNPauseIcon() {
   const icon = playNpauseBtn.querySelector("i");
@@ -26,3 +27,15 @@ video.addEventListener("play", updatePlayNPauseIcon);
 video.addEventListener('click', playNpauseFn)
 video.addEventListener("pause", updatePlayNPauseIcon);
 playNpauseBtn.addEventListener("click", playNpauseFn);
+
+
+// Rewind & Fast-forward functionality
+const rewindBtn = document.querySelector("#rewind");
+const fastForwardBtn = document.querySelector("#fast-forward");
+
+function rewindNforwardFn(type) {
+  video.currentTime += type === "rewind" ? -10 : 10;
+}
+
+rewindBtn.addEventListener("click", () => rewindNforwardFn("rewind"));
+fastForwardBtn.addEventListener("click", () => rewindNforwardFn("forward"));
